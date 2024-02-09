3def0dd339480b616d07fb0e22dd33dfcb443d7d
phamrina <phamrina@mail.uc.edu>
2024-02-09 15:18:39 -0500

    edit phone.js

--- phone.js
+++ phone.js
@@ -1,11 +1,12 @@
 var shown = false;
 function showhidePhone(){
 	if (shown){
-		document.getElementById('phone').innerHTML = "Show my phone number";
+		document.getElementById('phone').innerHTML = "Show my LinkedIn";
 		shown = false;
 	}else {
-		var myphone = "<a href='mailto:513" + 
-					     "-400-9741'>513" + "-400-9741</a>";
+		var myphone = "<a href='https://www.linkedin.com" + 
+					     "/in/nickphamrider'>https://www.linkedin.com" + 
+					     "/in/nickphamrider</a>";
 		document.getElementById('phone').innerHTML = myphone;
 		shown = true;
 	}
